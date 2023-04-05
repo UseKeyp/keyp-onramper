e4a9b673c7e9ee13f38fbf2be8b40472b402e155
geovgy <gbh218@gmail.com>
2023-04-05 15:44:27 -0400

    Update key config

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import { config } from 'dotenv'
 config()
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function saveResponse(requestUrl, responseData) {
