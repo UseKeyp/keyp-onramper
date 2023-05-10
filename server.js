560ee0773f0a87e6513aa6dc2859d74f41a1510f
geovgy <gbh218@gmail.com>
2023-05-10 13:51:43 -0400

    Add checkoutUrl POST and body parsing

--- server.js
+++ server.js
@@ -5,26 +5,28 @@ import { saveResponse } from "./database.js";
 
 config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/limits", async (req, res) => {
   const url = "https://api.onramper.com/supported/limits/moonpay/eur/eth/creditcard";
   const response = await fetch(url);
   const data = await response.json();
-  res.json(data);
+  return res.json(data);
 })
 
 app.get("/getAllCurrencies", async (req, res) => {
   const url = "https://api.onramper.com/supported";
   const response = await fetch(url);
   const data = await response.json();
-  res.json(data);
+  return res.json(data);
 })
 
 app.get("/getPaymentTypes", async (req, res) => {
   const url = "https://api.onramper.com/supported/payment-types/moonpay/eur/btc";
   const response = await fetch(url);
   const data = await response.json();
-  res.json(data);
+  return res.json(data);
 })
 
 app.get("/getQuote", async (req, res) => {
@@ -41,7 +43,7 @@ app.get("/getQuote", async (req, res) => {
   console.log(data)
   await saveResponse(url, data);
 
-  res.json(data);
+  return res.json(data);
 })
 
 app.post("/checkoutintent", async (req, res) => {
@@ -75,7 +77,7 @@ async function getBestQuote({
     }
   });
   const quotes = await response.json();
-  console.log("quotes:", quotes)
+  // console.log("quotes:", quotes)
   
   const sorted = quotes.sort((a, b) => -1*(a.payout - b.payout));
   const bestQuote = sorted[0];
@@ -91,8 +93,10 @@ app.get("/getCheckoutUrl", async (req, res) => {
       Authorization: process.env.ONRAMPER_API_KEY
     }
   });
-  const { crypto } = (await supported.json()).message
-  const cryptoId = crypto.find(c => c.symbol === currency && c.network === network).id;
+  const { crypto: cryptos } = (await supported.json()).message
+  const crypto = cryptos.find(c => c.code.toLowerCase() === currency && c.network.toLowerCase() === network);
+  console.log("crypto:", crypto)
+  const cryptoId = crypto?.id;
 
   const bestQuote = await getBestQuote({
     source: fiat||"usd",
@@ -103,8 +107,61 @@ app.get("/getCheckoutUrl", async (req, res) => {
     wallet: address||"",
     network: network||"ethereum"
   });
+  console.log("Best quote:", bestQuote)
   
   return res.json(bestQuote);
+})
+
+app.post("/getCheckoutUrl", async (req, res) => {
+  const { amount, fiat, currency, paymentMethod, network, address } = req.body;
+  
+  const supported = await fetch("https://api.onramper.com/supported", {
+    method: "GET",
+    headers: {
+      Authorization: process.env.ONRAMPER_API_KEY
+    }
+  });
+  const { crypto: cryptos } = (await supported.json()).message
+  const crypto = cryptos.find(c => c.code.toLowerCase() === currency && c.network.toLowerCase() === network);
+  console.log("crypto:", crypto)
+  const cryptoId = crypto?.id;
+
+  const bestQuote = await getBestQuote({
+    source: fiat,
+    destination: cryptoId,
+    amount: amount,
+    type: "buy",
+    paymentMethod: paymentMethod,
+    wallet: address,
+    network: network
+  });
+  console.log("Best quote:", bestQuote)
+
+  const url = "https://api.onramper.com/checkout/intent";
+  const payload = {
+    onramp: bestQuote.ramp,
+    source: fiat,
+    destination: cryptoId,
+    amount,
+    type: "buy",
+    paymentMethod,
+    wallet: address,
+    network: network
+  }
+  console.log("payload:", payload)
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": process.env.ONRAMPER_API_KEY
+    },
+    body: JSON.stringify(payload)
+  })
+  
+  console.log("status:", response.status, response.statusText)
+  const result = await response.json();
+  console.log("response:", result)
+  return res.json(result);
 })
 
 app.listen(3000, () => {
