e52175de8774b7a69402e3170a14b45af0af434c
Konrad Gnat <konradmgnat@gmail.com>
2023-05-25 20:14:17 +0800

    keyp onramp js widget and react widget

--- frontend/src/pages/Onramper/index.js
+++ frontend/src/pages/Onramper/index.js
@@ -1,33 +1,14 @@
-import { useSearchParams } from 'react-router-dom';
+import { KeypOnrampWidget } from '../../components/KeypOnrampWidget';
 import "./index.css";
+import KeypLogo from '../../images/keyp-logo.svg';
 
 export const Onramper = () => {
-  const [searchParams] = useSearchParams();
-  const currency = searchParams.get('currency');
-  const network = searchParams.get('network'); 
-  const currentUserAddress = searchParams.get('address');
-
-  console.log('onramper page')
-  console.log(currency, network, currentUserAddress);
-  // prevent loading of iframe if any of the required params are missing
-  if (!currency || !network || !currentUserAddress) {
-    return (
-      <div>
-        <h1 style={{ color: 'red', textAlign: 'center', padding: '2rem' }}>Error: you must include currency, network and address URL parameters</h1>
-      </div>
-    )
-  }
 
   return (
-    <div className="onramper-container">
-      <iframe
-        style={{ borderRadius: '4px', border: '1px solid #58585f', margin: 'auto', maxWidth: '420px' }}
-        src={`https://buy.onramper.com/?apiKey=${process.env.REACT_APP_ONRAMPER_API_KEY}&onlyCryptos=${currency}&onlyNetwork=${network}&isAddressEditable=false&wallets=ETH:${currentUserAddress}`}
-        height="630px"
-        width="420px"
-        title="Onramper widget"
-        allow="accelerometer; autoplay; camera; gyroscope; payment">
-      </iframe>
+    <div className="demo-container">
+      <img src={KeypLogo} className="demo-logo" alt="keyp logo"/>
+      <h1 className="demo-title">Keyp Onramp React.js Demo</h1>
+      <KeypOnrampWidget />
     </div>
   );
 }
